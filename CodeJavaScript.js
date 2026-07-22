4ded6b1ac2ad902053870784eb96f0483fa3669e
Yson <ysondantasdo@gmail.com>
2026-07-22 11:05:19 +0000

    Aula 6

--- CodeJavaScript.js
+++ CodeJavaScript.js
@@ -1,2 +1,17 @@
-var nome = window.prompt("Qual o seu nome ?")
-window.alert ("Seu nome: " + nome + "!")
+//var nome = window.prompt("Qual o seu nome ?")
+//window.alert ("Seu nome: " + nome + "!")
+//var n1 = Number.parseInt(window.prompt("N1?"))
+// se digitar 3.3 em n1 vai aramazenar só 3
+//var n2 = Number.parseFloat(window.prompt("N2?"))
+// se colocar só Number sem parse ele decide na hora
+//var s = n1 + n2
+//window.alert("soma é: " + s)
+//window.alert(`nota n1: ${n1} e o valor n2: ${n2} e a soma é: ${s}`)
+var texto ='Javascript'
+document.write(`<p class="linha1">Javascript letras: ${texto.length}</p><br>`)
+document.write("<p class='linha2'>Texto no class linha2 </p><br>")
+// em vez de usar br no final <br>
+// pode usar uma linha para o br
+//documento.write('<br>')
+window.alert(`tudo em maiúsculo' ${texto.toUpperCase()}`)
+document.write(`<h2>minúsculo: ${texto.toLowerCase()}</h2>`)
